376cc8a147df66ca58c76d5f44fe7983bf2016c8
tharunkumar1500 <tharunkumar13072@gmail.com>
2026-06-22 11:01:28 +0530

    chore: initialize plain java project structure

---
+++ src/quantitymeasurement/QuantityMeasurementApp.java
@@ -0,0 +1,7 @@
+package quantitymeasurement;
+
+public class QuantityMeasurementApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Quantity Measurement App!");
+    }
+}
